a2a8dd2c5165a2205ed696a1d8d4f06fe9688e0f
mobarak10 <mobarak.hossain.cse12@gmail.com>
2019-11-26 11:53:06 +0600

    vue basic course

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+Vue.config.productionTip = false;
+Vue.component("alert", require('./components/alert.vue').default);
+
+// Define a new component called button-counter
+Vue.component('button-counter', {
+    data: function () {
+      return {
+        count: 0
+      }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  })
+  
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
